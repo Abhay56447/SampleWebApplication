cf5c63af9a9e54894b36906ef7f73f8693be1e50
abhay kulkarni <agkulkarni.1310@gmail.com>
2025-12-18 08:10:21 +0530

    CatergoryController and Index,Create, Edit views added.

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SampleWebApplication.Data;
+using SampleWebApplication.Models;
+
+namespace SampleWebApplication.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> categoryList = _db.Categories.ToList();
+            return View(categoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            if (category != null)
+            {
+                _db.Categories.Add(category);
+                _db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            Category? category = _db.Categories.FirstOrDefault(c => c.Id == id);
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (category != null)
+            {
+                _db.Categories.Update(category);
+                _db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? category = _db.Categories.FirstOrDefault(c => c.Id == id);
+            _db.Categories.Remove(category);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
--- Migrations/20251217162651_SeedCategoryTable.cs
+++ Migrations/20251217162651_SeedCategoryTable.cs
@@ -7,7 +7,7 @@
 namespace SampleWebApplication.Migrations
 {
     /// <inheritdoc />
-    public partial class InitialCreate : Migration
+    public partial class SeedCategoryTable : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
